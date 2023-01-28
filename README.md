b75dde01d2586ca14dfb3f097d60b8ce80c9b23a
chris <er1293545211@live.com>
2023-01-28 23:21:57 +0800

    edit

---
+++ README.md
@@ -0,0 +1,6 @@
+https://trenki2.github.io/blog/2017/07/04/using-sdl2-image-with-cmake/
+https://github.com/tcbrindle/sdl2-cmake-scripts
+https://trenki2.github.io/blog/2017/06/02/using-sdl2-with-cmake/
+
+
+https://www.matsson.com/prog/sdl2-mingw-w64-tutorial.php
---
+++ src/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <SDL2/SDL.h>
+
+/*int main(int argc, char *argv[]) {
+    printf("main\n");
+    printf("argc: %d\n", argc);
+
+    if (argv != NULL) {
+        // 获得数组长度
+        printf("argv: %d\n", sizeof(*argv) / sizeof(argv[0]));
+    } else {
+        printf("argv is null");
+    }
+    return 0;
+}*/
+
+
+int main(int argc, char **argv) {
+    printf("main\n");
+    return 0;
+}
+
