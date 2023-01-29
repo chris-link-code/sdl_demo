7af08a603f5d486f4c720a5c8d1e836899185c63
chris <er1293545211@live.com>
2023-01-29 10:06:39 +0800

    ok

--- src/main.c
+++ src/main.c
@@ -1,22 +1,8 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
 
-/*int main(int argc, char *argv[]) {
-    printf("main\n");
-    printf("argc: %d\n", argc);
-
-    if (argv != NULL) {
-        // 获得数组长度
-        printf("argv: %d\n", sizeof(*argv) / sizeof(argv[0]));
-    } else {
-        printf("argv is null");
-    }
-    return 0;
-}*/
-
-
-int main(int argc, char **argv) {
-    printf("main\n");
+int WinMain(int argc, char **argv) {
+    printf("sdl demo\n");
     return 0;
 }
 
